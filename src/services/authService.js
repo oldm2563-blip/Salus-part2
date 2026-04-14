ef731ff43b41dc639b4f3174ad1170972d4b7b7d
Oldman2563 <oldm2563@gmail.com>
2026-04-14 21:18:18 +0100

    make and authService to connect the api

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,13 @@
 import axios from "axios";
+import { data } from "react-router-dom";
 
 const APP_URL = "http://16.171.43.223/api";
 
 
 export const register = (data) => axios.post(APP_URL + '/register', data);
 export const login = (data) => axios.post(APP_URL + '/login', data);
+export const profile = (token) => axios.get(APP_URL + '/me' , {
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+})
