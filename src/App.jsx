df2032711d8c338b6f4122f587430c1994590901
Oldman2563 <oldm2563@gmail.com>
2026-04-14 21:14:22 +0100

    add protectedroutes and add the token verifying

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 function App() {
+  const isAuth = JSON.parse(localStorage.getItem('token'))
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/Register" element={<Register />} />
+          <Route path="/login" element={isAuth ? <Navigate to='/Dashboard'/> : <Login />} />
+          <Route path="/Register" element={isAuth ? <Navigate to='/Dashboard'/> : <Register />} />
+
+          <Route element={<ProtectedRoutes/>}>
+              <Route path="/Dashboard" element={<Dashboard />} />
+          </Route>
+          
         </Routes>
       </BrowserRouter>
     </>
