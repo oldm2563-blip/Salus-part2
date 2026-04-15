c0e390b797534bc5bd21b017a64203b4fae98bc0
Miss Shahd <mazouzchahd12@gmail.com>
2026-04-15 11:03:24 +0100

    feat: add routers path and toast to the function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import SymptomList from "./pages/SymptomList";
+import SymptomList from "./pages/SymptomList.jsx";
+import AddSymptom from "./pages/AddSymptom.jsx";
+import { Route,BrowserRouter,Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import './App.css'
 
 function App() {
   return (
     <>
-      <SymptomList />
+    <BrowserRouter>
+    <Routes>
+      <Route  path="/SymptomList" element={<SymptomList />} />
+      <Route  path="/AddSymptom" element={<AddSymptom />} />
+    </Routes>
+    </BrowserRouter>
+    <ToastContainer />  
     </>
   );
 }
