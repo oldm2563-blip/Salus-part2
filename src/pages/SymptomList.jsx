0058ca1c420e23ccb837fb4d35e4a25e24fa8575
Miss Shahd <mazouzchahd12@gmail.com>
2026-04-15 11:07:46 +0100

    feat: create the crud for showing the list of symptoms

--- src/pages/SymptomList.jsx
+++ src/pages/SymptomList.jsx
@@ -1,30 +1,62 @@
 import { useEffect, useState } from "react";
 import api from "../services/api.js";
-function SymptomList(){
-const [symptoms, setSymptoms] = useState([]);
+import EditSymptom from "./EditSymptom.jsx";
+import DeleteSymptom from "./DeleteSymptom.jsx";
 
-useEffect(() => {
-  api.get("/symptoms")
-    .then(res => {
-      console.log(res.data);
-      setSymptoms(res.data.data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-}, []);
+function SymptomList() {
+  const [symptoms, setSymptoms] = useState([]);
+  const [editingSymptom, setEditingSymptom] = useState(null);
 
-return (
+  const fetchSymptoms = () => {
+    api.get("/symptoms")
+      .then(res => {
+        setSymptoms(res.data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchSymptoms();
+  }, []);
+
+  return (
     <div>
-        <h1>Symptoms</h1>
+      <h1>Symptoms</h1>
 
-        {symptoms.map((s) => (
-            <div key={s.id}>
-                <h3>{s.name}</h3>
-                <p>{s.severity}</p>
-            </div>
-        ))}
+      {editingSymptom && (
+        <EditSymptom
+          symptom={editingSymptom}
+          onSuccess={() => {
+            setEditingSymptom(null);
+            fetchSymptoms();
+          }}
+        />
+      )}
+
+      
+      {symptoms.map((s) => (
+        <div key={s.id} style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}>
+          <h1>{s.id}</h1>
+          <h3>{s.name}</h3>
+          <p>{s.severity}</p>
+          <p>{s.description}</p>
+
+          
+          <button onClick={() => setEditingSymptom(s)}>
+            Edit
+          </button>
+
+          
+          <DeleteSymptom
+            id={s.id}
+            onSuccess={fetchSymptoms}
+          />
+        </div>
+      ))}
     </div>
-);
+  );
 }
+
 export default SymptomList;
