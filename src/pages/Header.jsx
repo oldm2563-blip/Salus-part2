331c370da1b8cf91469c70f2e5b9f24bef79d7b9
redouane <benbrahimredouane664@gmail.com>
2026-04-16 16:08:48 +0100

    chore: change component name to header and add navigation to all component

---
+++ src/pages/Header.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+
+function Header() {
+  return (
+    <nav>
+      {" | "}
+      <Link to="/Dashboard"> Home</Link>
+      {" | "}
+      <Link to="/doctors">Doctors List</Link>
+      {" | "}
+      <Link to="/search"> Search A Doctor</Link>
+
+      {" | "}
+      <Link to="/SymptomList">SymptomList List</Link>
+      {" | "}
+      <Link to="/AddSymptom">AddSymptom</Link>
+      {" | "}
+      <Link to="/Advice_history"> Advice_history</Link>
+       {" | "}
+      <Link to="/Generate_advice"> Generate_advice</Link>
+       {" | "}
+      <Link to="/Create_appointments"> Create_appointments</Link>
+    </nav>
+  );
+}
+export default Header;
